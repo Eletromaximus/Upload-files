e855928ed1574aeeaf1766f38f3e9cc1699890d7
Eletromaximus <maxmillianox@protonmail.com>
2020-05-22 21:16:00 -0300

    Configurando multer e primeiro teste de upload com isomnia

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
 "use strict";
-var morgan = require("morgan");
-var express = require("express");
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extend: true }));
-app.use(morgan('dev'));
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var morgan_1 = __importDefault(require("morgan"));
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(morgan_1.default('dev'));
+app.get('/', function (req, res) {
+    return res.send('Hello World 1');
+});
 app.use(require("./routes"));
 app.listen(3000);
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
-const routes = require('express').Router();
+import {Router} from "express";
+import multer from "multer";
 
-routes.get('/', (req:Request, res:Response) =>{
-    return res.json();
+const routes = Router();
+const multerConfig = require("./config/multer");
+
+routes.post('/posts', multer(multerConfig).single("file"), (req, res) =>{
+    console.log(req.file);
+    
+    return res.json({hello: "Rocket"});
 });
 
 module.exports = routes;
