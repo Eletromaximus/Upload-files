fac1f920bc1e8933b896eb7ac7eb9c82914a6225
Eletromaximus <maxmillianox@protonmail.com>
2020-05-24 22:17:44 -0300

    Upload em disco com o multer e preparação do docker.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import morgan from 'morgan';
-
 import express from 'express';
 
+//const mongoose = require('mongoose');
 const app = express();
 
+
+/*mongoose.connect('mongodb://localhost:27017/upload', {
+    useNewUrlParser: true,
+})*/
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
